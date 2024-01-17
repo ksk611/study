7f3f14e3aed901569f8a1c61753f7ab91ecd22df
ksk611 <oppllii@naver.com>
2024-01-17 10:45:21 +0900

    1

--- BasicJava/src/kr/or/ddit/study11/DateExample.java
+++ BasicJava/src/kr/or/ddit/study11/DateExample.java
@@ -1,5 +1,99 @@
 package kr.or.ddit.study11;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class DateExample {
+	
+	static boolean deb = true;
+	
+	public static void main(String[] args) {
+		DateExample de = new DateExample();
+//		de.method1();
+//		de.method2();
+		de.method3();
+	}
+
+	private void method3() {
+		/*
+		 * SimpleDateFormat
+		 * String -> Date
+		 * Date -> String
+		 * 
+		 * Date 객체를 원하는 포맷으로 변경하는 클래스 
+		 * 
+		 * 2024/01/17
+		 * 2024.01.17
+		 * 2024-01-17
+		 * 
+		 */
+		
+		//소문자 m은 분을 의미
+		//대문자 M은 달을 의미
+		Date d = new Date();
+		SimpleDateFormat sdf= new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+		String result = sdf.format(d);
+		
+		System.out.println(result);
+		
+	}
+
+	private void method2() {
+		/*
+		 * 1970 년으로 부터 얼마나 지났는지 
+		 * 년 일 시 분 초로 표시
+		 */
+		Date d = new Date();
+		long time = d.getTime()/1000;
+		
+		int min = 60;
+		int hour = min*60;
+		int day = hour*24;
+		int year = day*365;
+		
+		int years =(int)(time/year);
+		System.out.println(years);
+		
+		time%=year;
+		
+		int days =(int)(time/day);
+		System.out.println(days);
+		time%=day;
+		
+		int hours =(int)(time/hour);
+		System.out.println(hours);
+		time%=hour;
+		
+		int mins =(int)(time/min);
+		System.out.println(mins);
+		time%=min;
+		
+		System.out.println(time);
+		
+		System.out.println(years+"년"+days+"일"+ hours+"시간"+mins+"분"+time+"초");
+		
+	}
+
+	private void method1() {
+		
+		Date d1 = new Date();
+		System.out.println(d1);
+		
+		try {
+			Thread.sleep(3000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		Date d2 = new Date();
+		System.out.println(d2);
+		
+		long time = d2.getTime()-d1.getTime();
+		//d2와 d1의 시간 차이 단위 ms
+		System.out.println(time/1000);
+		
+		System.out.println(d2.getTime());
+		System.out.println(d1.getTime());
+	}
 
 }
